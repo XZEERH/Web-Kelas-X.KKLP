3f7b63e6147659b9fc80e43d7761ed258a6e8b87
XZEERH <razzyr408@gmail.com>
2026-04-26 17:40:40 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import JadwalPiket from './components/JadwalPiket';
 import AnggotaInteraktif from './components/AnggotaInteraktif';
 import MomentGallery from './components/MomentGallery';
 import About from './components/About';
-import Footer from './components/Footer'; // Import Footer yang baru
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="antialiased selection:bg-blue-600 selection:text-white">
       <Navbar />
       <main>
-        <Hero />
-        <JadwalPiket />
-        <AnggotaInteraktif />
-        <MomentGallery />
-        <About />
+        {/* Tambahkan id agar navigasi berfungsi */}
+        <section id="home"><Hero /></section>
+        <section id="piket"><JadwalPiket /></section>
+        <section id="anggota"><AnggotaInteraktif /></section>
+        <section id="moment"><MomentGallery /></section>
+        <section id="tentang"><About /></section>
       </main>
-      <Footer /> {/* Render Footer di sini */}
+      <Footer />
     </div>
   );
 }
